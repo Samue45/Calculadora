2f91e3349274e71cb3d0afee3a0f25aeeafd7a30
samuelayllonsevilla86@gmail.com <samuelayllonsevilla86@gmail.com>
2025-03-25 13:44:43 +0100

    Calculadora

--- src/main/java/com/example/demo/Servicio/Servicio.java
+++ src/main/java/com/example/demo/Servicio/Servicio.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class Servicio extends VerticalLayout {
 
-    // Patrón Singleton
+    // -Patrón Singleton
     private static Servicio servicio;
 
     // Propiedades del Servicio
